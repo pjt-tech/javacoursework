ab59a611473ea97f8316fd04a28faf68659d2198
jintae_park <dktkd7044@gmail.com>
2021-12-15 23:45:10 +0900

    상수 , 연산자 실습

---
+++ src/chapter10/ContantTest.java
@@ -0,0 +1,14 @@
+package chapter10;
+
+public class ContantTest {
+
+    public static void main(String[] args) {
+
+        final int MAX_NUM = 100;
+        final int MIN_NUM = 1;
+
+        System.out.println("MAX_NUM = " + MAX_NUM);
+        System.out.println("MIN_NUM = " + MIN_NUM);
+
+    }
+}
