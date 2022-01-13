a6d01b6df64bfa0d039bdd7775b831b9929a338c
jintae_park <dktkd7044@gmail.com>
2022-01-13 14:27:14 +0900

    인터페이스와 상속 실습

---
+++ src/chapter3/ch15/CustomerTest.java
@@ -0,0 +1,20 @@
+package chapter3.ch15;
+
+public class CustomerTest {
+
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        customer.buy();
+        customer.sell();
+        customer.order();
+        customer.hello();
+
+        Buy buyer = new Customer();
+        buyer.buy();
+        buyer.order();
+
+        Sell seller = new Customer();
+        seller.order();
+        seller.sell();
+    }
+}
